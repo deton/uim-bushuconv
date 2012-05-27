114ecda2a121a0ea402d72e2ac862e166a6579ed
KIHARA Hideto <deton@m1.interq.or.jp>
2012-05-27 10:57:44 +0900

    Remove bushuconv-proc-state-interactive-bushu.

--- bushuconv.scm
+++ bushuconv.scm
@@ -63,121 +63,6 @@
   (im-deactivate-candidate-selector pc)
   (tutcode-release-handler (bushuconv-context-tc pc)))
 
-;;; copy and modified from tutcode-proc-state-interactive-bushu
-(define (bushuconv-proc-state-interactive-bushu c key key-state)
-  (let*
-    ((pc (tutcode-find-descendant-context c))
-     (rkc (tutcode-context-rk-context pc))
-     (head (tutcode-context-head pc))
-     (res #f)
-     (has-candidate? (> (tutcode-context-prediction-nr pc) 0))
-     ;; 候補表示のページ移動時は、reset-candidate-windowしたら駄目
-     (candidate-selection-keys-handled?
-      (if has-candidate?
-        (cond
-          ((tutcode-next-page-key? key key-state)
-            (tutcode-change-prediction-page pc #t)
-            #t)
-          ((tutcode-prev-page-key? key key-state)
-            (tutcode-change-prediction-page pc #f)
-            #t)
-          ((and (tutcode-next-candidate-key? key key-state)
-                ;; 2打鍵目のスペースキーの場合は候補選択ではない
-                (= (length (rk-context-seq rkc)) 0))
-            (tutcode-change-prediction-index pc 1)
-            #t)
-          ((tutcode-prev-candidate-key? key key-state)
-            (tutcode-change-prediction-index pc -1)
-            #t)
-          (else
-            #f))
-        #f)))
-    (if (not candidate-selection-keys-handled?)
-      (begin
-        (tutcode-reset-candidate-window pc)
-        (cond
-          ((tutcode-off-key? key key-state)
-           (tutcode-flush pc)
-           (tutcode-context-set-prediction-nr! pc 0)
-           (tutcode-context-set-state! pc 'tutcode-state-off))
-          ;((and (tutcode-kana-toggle-key? key key-state)
-          ;      (not (tutcode-kigou2-mode? pc)))
-          ; (rk-flush rkc)
-          ; (tutcode-context-kana-toggle pc))
-          ;((tutcode-kigou2-toggle-key? key key-state)
-          ; (rk-flush rkc)
-          ; (tutcode-toggle-kigou2-mode pc))
-          ((tutcode-backspace-key? key key-state)
-           (if (> (length (rk-context-seq rkc)) 0)
-            (rk-flush rkc)
-            (if (> (length head) 0)
-              (begin
-                (tutcode-context-set-head! pc (cdr head))
-                (if has-candidate?
-                  (tutcode-begin-interactive-bushu-conversion pc))))))
-          ((or
-            (tutcode-commit-key? key key-state)
-            (tutcode-return-key? key key-state))
-           (let ((str
-                  (cond
-                    (has-candidate?
-                      (tutcode-get-prediction-string pc
-                        (tutcode-context-prediction-index pc))) ; 熟語ガイド無
-                    ((> (length head) 0)
-                      (string-list-concat (tutcode-context-head pc)))
-                    (else
-                      #f))))
-             (if str (tutcode-commit pc str))
-             (tutcode-flush pc)
-             (tutcode-context-set-prediction-nr! pc 0)
-             (if str (tutcode-check-auto-help-window-begin pc (list str) ()))))
-          ((tutcode-cancel-key? key key-state)
-           (tutcode-context-set-prediction-nr! pc 0)
-           (tutcode-flush pc))
-          ((tutcode-stroke-help-toggle-key? key key-state)
-           (tutcode-toggle-stroke-help pc))
-          ((tutcode-paste-key? key key-state)
-            (let ((latter-seq (tutcode-clipboard-acquire-text-wo-nl pc 'full)))
-              (if (pair? latter-seq)
-                (begin
-                  (tutcode-context-set-head! pc (append latter-seq head))
-                  (tutcode-begin-interactive-bushu-conversion pc)))))
-          ((or
-            (symbol? key)
-            (and
-              (modifier-key-mask key-state)
-              (not (shift-key-mask key-state))))
-           (tutcode-context-set-prediction-nr! pc 0)
-           (tutcode-flush pc)
-           ;(tutcode-proc-state-on pc key key-state)
-           )
-          ((and (tutcode-heading-label-char-for-prediction? key)
-                (= (length (rk-context-seq rkc)) 0)
-                (tutcode-commit-by-label-key-for-prediction pc
-                  (charcode->string key)
-                  'tutcode-predicting-interactive-bushu)))
-          ((not (rk-expect-key? rkc (charcode->string key)))
-           (if (> (length (rk-context-seq rkc)) 0)
-             (begin
-               (if (tutcode-verbose-stroke-key? key key-state)
-                 (set! res (last (rk-context-seq rkc))))
-               (rk-flush rkc))
-             (set! res (charcode->string key))))
-          (else
-           (set! res (tutcode-push-key! pc (charcode->string key)))))
-        (cond
-          ((string? res)
-            (tutcode-append-string pc res)
-            (tutcode-begin-interactive-bushu-conversion pc))
-          ((symbol? res)
-           (case res
-            ((tutcode-auto-help-redisplay)
-              (tutcode-auto-help-redisplay pc))
-            ;;XXX 部首合成変換中は交ぜ書き変換等は無効にする
-            ))
-          ((procedure? res)
-           (res 'tutcode-state-interactive-bushu pc)))))))
-
 (define (bushuconv-key-press-handler pc key key-state)
   (if (ichar-control? key)
     (im-commit-raw pc)
@@ -186,8 +71,12 @@
         ((bushuconv-switch-default-im-key? key key-state)
           (im-switch-im pc default-im-name))
         (else
-          (bushuconv-proc-state-interactive-bushu tc key key-state)
-          (tutcode-context-set-state! tc 'tutcode-state-interactive-bushu)
+          (tutcode-proc-state-interactive-bushu tc key key-state)
+          (if (not (eq? (tutcode-context-state tc)
+                        'tutcode-state-interactive-bushu)) ;; after commit
+            (begin
+              (tutcode-context-set-state! tc 'tutcode-state-interactive-bushu)
+              (tutcode-context-set-prediction-nr! tc 0)))
           (tutcode-update-preedit tc)
           (tutcode-check-stroke-help-window-begin tc))))))
 
