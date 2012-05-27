344907c30f493c52d7d3ebe1c5849bb74d0a4bc1
KIHARA Hideto <deton@m1.interq.or.jp>
2012-05-27 11:08:48 +0900

    Support bushuconv-switch-default-im-after-commit.

--- bushuconv.scm
+++ bushuconv.scm
@@ -33,8 +33,6 @@
 (require "tutcode.scm")
 (require-custom "bushuconv-custom.scm")
 
-(define bushuconv-switch-default-im-key? (make-key-predicate '("<IgnoreShift>~")))
-
 (define bushuconv-context-rec-spec
   (append
     context-rec-spec
@@ -74,9 +72,11 @@
           (tutcode-proc-state-interactive-bushu tc key key-state)
           (if (not (eq? (tutcode-context-state tc)
                         'tutcode-state-interactive-bushu)) ;; after commit
-            (begin
-              (tutcode-context-set-state! tc 'tutcode-state-interactive-bushu)
-              (tutcode-context-set-prediction-nr! tc 0)))
+            (if bushuconv-switch-default-im-after-commit
+              (im-switch-im pc default-im-name)
+              (begin
+                (tutcode-context-set-state! tc 'tutcode-state-interactive-bushu)
+                (tutcode-context-set-prediction-nr! tc 0))))
           (tutcode-update-preedit tc)
           (tutcode-check-stroke-help-window-begin tc))))))
 
