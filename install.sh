caec3e5048083f25c8ef35360be181eadae2870b
KIHARA Hideto <deton@m1.interq.or.jp>
2012-06-09 11:58:21 +0900

    Add icons and install.sh.

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+module=bushuconv
+srcdir=$(dirname $0)
+scmdir=$(pkg-config --variable=uim_scmdir uim)
+pixmapsdir=$(pkg-config --variable=uim_datadir uim)/pixmaps
+cp "$srcdir/$module.scm" "$srcdir/$module-custom.scm" "$scmdir"
+cp "$srcdir/pixmaps/$module.png" "$srcdir/pixmaps/${module}_dark_background.png" "$pixmapsdir"
+cp "$srcdir/pixmaps/$module.svg" "$srcdir/pixmaps/${module}_dark_background.svg" "$pixmapsdir"
+uim-module-manager --register "$module"
