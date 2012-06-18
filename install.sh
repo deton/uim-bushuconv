a83255922d43d6a30d7f9e17e1214ab3d8bdf4ef
KIHARA Hideto <deton@m1.interq.or.jp>
2012-06-18 21:29:17 +0900

    Fix missing quote.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ module=bushuconv
 srcdir=$(dirname $0)
 scmdir=$(pkg-config --variable=uim_scmdir uim)
 pixmapsdir=$(pkg-config --variable=uim_datadir uim)/pixmaps
-cp "$srcdir/$module.scm" "$srcdir/$module-custom.scm" $srcdir/$module-rule.scm" "$scmdir"
+cp "$srcdir/$module.scm" "$srcdir/$module-custom.scm" "$srcdir/$module-rule.scm" "$scmdir"
 cp "$srcdir/pixmaps/$module.png" "$srcdir/pixmaps/${module}_dark_background.png" "$pixmapsdir"
 cp "$srcdir/pixmaps/$module.svg" "$srcdir/pixmaps/${module}_dark_background.svg" "$pixmapsdir"
 uim-module-manager --register "$module"
