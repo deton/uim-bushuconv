ee3093b409f8a131eaf3d4d936d22047217e4579
KIHARA Hideto <deton@m1.interq.or.jp>
2012-08-19 19:23:53 +0900

    annotationファイルの変換元のUnihan_Readings.txtファイル取得コマンドをコメントした状態で追加

--- annotation/mkann.sh
+++ annotation/mkann.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+# wget -N http://unicode.org/Public/UNIDATA/Unihan.zip
+# unzip Unihan.zip
 /usr/bin/awk -f kunondef.awk Unihan_Readings.txt | \
 $PWD/kunonrk.scm | sed -e 's/^u+//' -e 's/	/ /g' | LANG=C sort >annotation
