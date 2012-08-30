3e841d6f32c47a29b4c1d61487782dd286a50afe
KIHARA Hideto <deton@m1.interq.or.jp>
2012-08-30 20:20:01 +0900

    Add test of uimsh-ucs.scm and uimsh-rk.scm Change expected value according to bushuconv-kanjiset custom variable.

--- test/test.sh
+++ test/test.sh
@@ -7,14 +7,60 @@ testsame () {
 	fi
 }
 
-# TODO: set bushuconv-kanjiset for this test.
-# (define bushuconv-kanjiset 'bushu34)
+kanjiset=`uim-sh -e '(begin (require "bushuconv.scm") bushuconv-kanjiset)'`
+case $kanjiset in
+bushu34)
+	bushu34=1
+	;;
+bushu34h+)
+	bushu34=0
+	;;
+*)
+	echo "Unknown bushuconv-kanjiset: $kanjiset"
+	exit 1
+	;;
+esac
 
 actual=`echo '木刀' | $PWD/../tools/uimsh-bushuconv.scm`
-testsame "朷枌枴梛棻棼楔檞梁棃𥱋樑簗蔾" "$actual"
+case $kanjiset in
+bushu34)
+	testsame "朷枌枴梛棻棼楔檞梁棃𥱋樑簗蔾" "$actual"
+	;;
+bushu34h+)
+	testsame "朷枌枴梛栔棻棼楔檞梁棃𥱋樑簗蔾" "$actual"
+	;;
+*)
+	echo "NG Unknown bushuconv-kanjiset: $kanjiset"
+	;;
+esac
 
 actual=`echo '口木イ' | $PWD/../tools/uimsh-bushuconv.scm`
-testsame "保堡葆褒褓" "$actual"
+case $kanjiset in
+bushu34)
+	testsame "保堡葆褒褓" "$actual"
+	;;
+bushu34h+)
+	testsame "咻保堡媬緥葆褒褓賲僺" "$actual"
+	;;
+*)
+	echo "NG Unknown bushuconv-kanjiset: $kanjiset"
+	;;
+esac
 
 $PWD/../tools/uimsh-kanjiset.scm <kanjiset.in >kanjiset.out
 cmp kanjiset.out kanjiset.expect && echo OK && rm kanjiset.out
+
+actual=`echo '例えば、U+8a3bU+91c8とか' | $PWD/../tools/uimsh-ucs.scm`
+testsame "例えば、註釈とか" "$actual"
+
+actual=`$PWD/../tools/uimsh-rk.scm ittyoume | iconv -f euc-jp -t utf-8`
+testsame "いっちょうめ" "$actual"
+
+actual=`$PWD/../tools/uimsh-rk.scm -k shoppingu | iconv -f euc-jp -t utf-8`
+testsame "ショッピング" "$actual"
+
+actual=`$PWD/../tools/uimsh-rk.scm shinnyuu | iconv -f euc-jp -t utf-8`
+testsame "しんゆう" "$actual"
+
+actual=`$PWD/../tools/uimsh-rk.scm -n shinnyuu | iconv -f euc-jp -t utf-8`
+testsame "しんにゅう" "$actual"
